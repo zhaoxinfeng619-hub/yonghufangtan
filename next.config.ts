2a9f35ebf502a4c9034db1c751668f05820428d4
赵鑫峰 <gaowenjie@MacBook-Pro-619.local>
2026-09-04 00:18:28 +0800

    支持 GitHub Pages 项目路径

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   output: isGithubPages ? "export" : undefined,
+  basePath: isGithubPages ? "/yonghufangtan" : undefined,
   trailingSlash: true,
 };
 
